97931580cbd26c33e773ff908d37763251dbcbfd
Paul Adutwum <pauladutwum303@gmail.com>
2026-07-26 15:26:20 -0400

    Update Next.js Version

--- waste-ui/.next/server/next-font-manifest.js
+++ waste-ui/.next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\n  \"app\": {},\n  \"appUsingSizeAdjust\": false,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/pauladutwum/Documents/Myprojects/RecycLens/waste-ui/src/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2","static/media/36966cca54120369-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
